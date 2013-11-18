6c90fae0da32cf8527b6cef7a519afcc2544c99c
Rob Jones <jones@craic.com>
2013-11-18 13:17:18 -0800

    tutorial 21

--- web_apprentice_demo_app.rb
+++ web_apprentice_demo_app.rb
@@ -65,27 +65,11 @@ class WebApprenticeDemoApp < Sinatra::Base
 
     # Get the Latitude and Longitude values and convert to decimal format
 
-
-# e.g 48 deg 20' 54.60" N
-    # def dms_to_decimal(coordinate)
-    #   decimal = 0.0
-    #   if coordinate =~ /^(\d)\s+deg\s+(\d+)\'\s+(\d+)"\s(\S)\s*$/
-    #     deg = $1.to_f
-    #     min = $2.to_f
-    #     sec = $3.to_f
-    #     direction = $4
-    #     decimal = deg + (min*60.0 + sec)/3600.0
-    #     if direction == 'S' or direction == 'W'
-    #       decimal = -decimal
-    #     end
-    #   end
-    #   decimal
-    # end
-
     @latitude  = dms_to_decimal(@photo['GPSLatitude'])
     @longitude = dms_to_decimal(@photo['GPSLongitude'])
-    # @latitude  = @photo['GPSLatitude']
-    # @longitude = @photo['GPSLongitude']
+
+    bearing = @photo['GPSImgDirection'].to_f
+    @latitude1, @longitude1 = latlong_offset(@latitude, @longitude, bearing, 0.025)
 
     erb :tutorial_22_demo_1
   end
@@ -97,18 +81,52 @@ end
 # 48 deg 20' 54.60" N
 # 122 deg 25' 25.20" W
 
-    def dms_to_decimal(coordinate)
-      decimal = 0.0
-      if coordinate =~ /^(\d+)\s+deg\s+(\d+)\'\s+([\d\.]+)\"\s+(\S)\s*$/
-        deg = $1.to_f
-        min = $2.to_f
-        sec = $3.to_f
-        direction = $4
-        decimal = deg + (min*60.0 + sec)/3600.0
-        if direction == 'S' or direction == 'W'
-          decimal = -decimal
-        end
-      end
-      decimal
+def dms_to_decimal(coordinate)
+  decimal = 0.0
+  if coordinate =~ /^(\d+)\s+deg\s+(\d+)\'\s+([\d\.]+)\"\s+(\S)\s*$/
+    deg = $1.to_f
+    min = $2.to_f
+    sec = $3.to_f
+    direction = $4
+    decimal = deg + (min*60.0 + sec)/3600.0
+    if direction == 'S' or direction == 'W'
+      decimal = -decimal
     end
+  end
+  decimal
+end
+
+def degrees_to_radians(angle)
+  angle * Math::PI / 180.0
+end
+
+def radians_to_degrees(angle)
+  angle * 180.0 / Math::PI
+end
+
+# This will work for short distances and not right at the poles
+def latlong_offset(lat0, lng0, bearing, distance)
+  rlat0 = degrees_to_radians(lat0)
+  rlng0 = degrees_to_radians(lng0)
+
+  rbearing = degrees_to_radians(bearing)
+
+  radius_of_earth = 6371.0  # km
+
+  ang_distance = distance / radius_of_earth
+
+  rlat1 = Math.asin( Math.sin(rlat0) * Math.cos(ang_distance) +
+                     Math.cos(rlat0) * Math.sin(ang_distance) * Math.cos(rbearing) )
+
+  rlng1 = rlng0 + Math.atan2(Math.sin(rbearing) * Math.sin(ang_distance) * Math.cos(rlat0),
+                             Math.cos(ang_distance) - Math.sin(rlat0) * Math.sin(rlat1))
+
+  # normalise to -180..+180º
+  rlng1 = (rlng1 + 3*Math::PI) % (2*Math::PI) - Math::PI
+
+  lat1 = radians_to_degrees(rlat1)
+  lng1 = radians_to_degrees(rlng1)
+
+  [ lat1, lng1 ]
+end
 
